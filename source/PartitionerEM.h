8ccbde74071a2383f8e7687c8928065d09b7abe9
Yutong Zhao <proteneer@gmail.com>
2013-08-11 17:38:50 -0700

    Added documentation for PartitionerEM concrete class

--- source/PartitionerEM.h
+++ source/PartitionerEM.h
@@ -6,6 +6,16 @@
 
 namespace Terran {
 
+/* Implements the Partitioner class using the expectation maximization algorithm.
+
+   By default, EM::multiAdaptiveRun() is used to fit a set of Gaussian Mixture Models
+   to the underlying data. The resulting minima of said curve is found, and the subset
+   of minima whose probability is less than the cutoff is set as the partition.
+   
+   For more fined tuned control of how the EM should be done,
+   such as setting initial gaussian mixture parameters, a reference to the underlying EM
+   object can be obtained.
+*/
 class PartitionerEM : public Partitioner {
 
 public:
@@ -16,13 +26,14 @@ public:
     // default method uses multiAdaptiveRun in EM.
     void optimizeParameters();
 
+    // invokes optimizeParameters and findLowMinima
     std::vector<double> partition();
     
     EM& getEM();
-
-    bool isPeriodic() const;
     
 private:
+
+    bool isPeriodic() const;
     
     std::vector<double> findLowMinima() const;
 
