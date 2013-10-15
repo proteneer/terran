a676b6580281761f7314301b39f45e769d9bd28f
Yutong Zhao <proteneer@gmail.com>
2013-10-14 21:12:28 -0700

    Better API cluster class

--- wrappers/terran.pyx
+++ wrappers/terran.pyx
@@ -26,42 +26,37 @@ cdef class PyCluster:
 
     # these are instance variables
     cdef Cluster* __thisptr
-    cdef int __autoDelete
+    cdef int __delete
     
-    def __init__(self):
+    def __cinit__(self, *args):
         """
-        Do not invoke publicly
+        Initializes a Cluster object.
+
+        Arguments:
+        Arg[0] -- a coordinates array of shape (N,D) where,
+                  N is the number of points, D is the number dimensions
+        Arg[1] -- a 1D boolean array of size D where,
+                  1 denotes the domain is periodic, and 0 denotes not periodic
         """
-        pass
-       
-    def __initFromArray(self, vector[vector[double]] data, vector[int] period):
-        self.__thisptr = new Cluster(data, period)
-        self.__autoDelete = 1
+        if len(args) == 2:
+            data = <vector[vector[double]]?> args[0]
+            period = <vector[int]?> args[1]
+            self.__thisptr = new Cluster(data, period)
+            self.__delete = 1
         
-    cdef __initFromRawPointer(self, Cluster *ptr):
-        self.__thisptr = ptr
-        self.__autoDelete = 0
-        
-    @classmethod
-    def fromArray(cls, vector[vector[double]] data, vector[int] period):
-        """
-        data - shape (N,D) where N is the number of points, D is the number dimensions
-        period - array of size D, allowed values of period[d] are 0 and 1, where:
-                                  if period[d] is 0 then dimension d is not periodic
-                                  if period[d] is 1 then dimension d is periodic 
-        """
-        instance = cls()
-        instance.__initFromArray(data, period)
-        return instance
-  
+    cdef __initFromRawPointer(self, Cluster* cptr):
+        self.__thisptr = cptr
+        self.__delete = 0
+
     def __dealloc__(self):
-        if self.__autoDelete:
+        if self.__delete:
             del self.__thisptr
             
     def partition(self, int d):
         """
-        Find deep valleys in marginalized distribution of dimension d and
-        returns the cutting point(s)
+        Find deep valleys in marginalized distribution of dimension d.
+
+        Returns a list of cutting point(s)
         """
         
         self.__thisptr.partition(d)
@@ -70,25 +65,29 @@ cdef class PyCluster:
     def assign(self):
         """
         Assign points to a given cluster (0-indexed). 
+
         Must be called after partition(d) has been invoked on each dimension
         """
         return self.__thisptr.assign()
-           
-    def set_subsample(self, int count):
-        """
-        Set the number of points to subsample when partitioning each dimension
-        """
-        self.__thisptr.setSubsampleCount(count)
-        
+                
     property subsample:
+        """ 
+        Mutable: the number of points to subsample when partitioning each dimension
+        """
         def __get__(self): return self.__thisptr.getSubsampleCount()
         def __set__(self, int s): self.__thisptr.setSubsampleCount(s)
     
     property shape:
+        """
+        Immutable: returns the shape of the underlying data
+        """
         def __get__(self): return self.__thisptr.getNumPoints(), self.__thisptr.getNumDimensions()
     
     @property
     def dimensions(self):
+        """
+        A generator used to loop over the dimensions
+        """
         d = 0
         while d < self.__thisptr.getNumDimensions():
             yield d
@@ -109,22 +108,28 @@ cdef class PyClusterTree:
     
     def __cinit__(self, vector[vector[double]] data, vector[int] period):
         """
-        data - shape (N,D) where N is the number of points, D is the number dimensions
-        period - array of size D, allowed values of period[d] are 0 and 1, where:
-                                  if period[d] is 0 then dimension d is not periodic
-                                  if period[d] is 1 then dimension d is periodic 
+        Initializes a Cluster object.
+
+        Arguments:
+        data   -- a coordinates array of shape (N,D) where,
+                  N is the number of points, D is the number dimensions
+        period -- a 1D boolean array of size D where,
+                  1 denotes the domain is periodic, and 0 denotes not periodic
         """
         self.__thisptr = new ClusterTree(data,period)
                 
     def __dealloc__(self):
         del self.__thisptr
-        
-    def __gcc(self):
+              
+    def get_current_cluster(self):
+        """
+        Returns a reference to the cluster object currently being investigated.
+
+        Note: The ownership of the object returned by this function belongs to the ClusterTree class.
+              You cannot take ownership of it and/or try to delete it.
+        """
         pyc = PyCluster()
         cdef Cluster* ptr = &(self.__thisptr.getCurrentCluster())
         pyc.__initFromRawPointer(ptr)
         return pyc
-        
-    def get_current_cluster(self):
-        return self.__gcc()
         
